ed524c49d83da4931c194bfd7591c2b1667b47cb
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-26 15:40:49 +0300

    add showing network space

--- ChiaBalance/Program.cs
+++ ChiaBalance/Program.cs
@@ -2,6 +2,7 @@
 using CHIA_RPC.General_NS;
 using CHIA_RPC.Wallet_NS.Wallet_NS;
 using Chia_Client_API.FullNodeAPI_NS;
+using CHIA_RPC.FullNode_NS;
 
 namespace ChiaBalance
 {
@@ -14,13 +15,15 @@ namespace ChiaBalance
 
         public static void Main()
         {
+            node = new FullNode_RPC_Client(reportResponseErrors: false);
             foreach (var item in Wallet.GetWallets_Sync().wallets)
             {
                 WalletID_RPC walletID_RPC = new WalletID_RPC(item.id);
                 GetWalletBalance_Response response = Wallet.GetWalletBalance_Sync(walletID_RPC);
                 Console.WriteLine(response.wallet_balance.confirmed_wallet_balance_in_xch);
-                var a = Wallet.GetWallets_Sync();
             }
+            var a = node.GetNetworkSpace_Sync(new GetNetworkSpace_RPC("0xf432a8f256f9da0cb695f29592140aeb98706c9b8a73881c52d152e9ba3cc769", "0x11419f3f0c741044ebf0ac4cb933b167a76617bd26484b3833056e1f3314c63e"));
+            Console.WriteLine(a.space_eb);
             Console.ReadKey();
         }
     }
