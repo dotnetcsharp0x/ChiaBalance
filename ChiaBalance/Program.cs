6b0921ee0337f030bae65e808af7d0f3f2eb8a79
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-26 15:03:07 +0300

    show balance of current wallet

--- ChiaBalance/Program.cs
+++ ChiaBalance/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ChiaBalance
+﻿using Chia_Client_API.WalletAPI_NS;
+using CHIA_RPC.General_NS;
+using CHIA_RPC.Wallet_NS.Wallet_NS;
+
+namespace ChiaBalance
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static Wallet_RPC_Client Wallet = new Wallet_RPC_Client(reportResponseErrors: false);
+
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            WalletID_RPC walletID_RPC = new WalletID_RPC(1);
+            GetWalletBalance_Response response = Wallet.GetWalletBalance_Sync(walletID_RPC);
+            Console.WriteLine(response.wallet_balance.confirmed_wallet_balance_in_xch);
+            Console.ReadKey();
         }
     }
 }
