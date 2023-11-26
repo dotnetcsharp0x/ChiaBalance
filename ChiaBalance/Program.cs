210e4b1828612bc1105e8d589b65ce4d588d90c2
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-26 15:01:48 +0300

    Добавьте файлы проекта.

---
+++ ChiaBalance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChiaBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
